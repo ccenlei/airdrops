06390b80da2ea87ee63db12d6baa726a0b79f826
ccenlei <iacc7441@yahoo.co.jp>
2023-04-24 16:26:47 +0900

    update

---
+++ base_chain_files/_Token_airdrop.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract AirdropAdventure { 
+    string public name = "My AI"; 
+    string public symbol = "MAI"; 
+    uint8 public decimals = 18; 
+    uint256 public totalSupply = 100000000;
+
+    mapping (address => uint256) public balances; 
+    address public owner;
+
+   constructor() { 
+       owner = msg.sender; 
+       balances[owner] = totalSupply; 
+    }
+
+    function transfer(address recipient, uint256 amount) public { 
+        require(balances[msg.sender] >= amount, "Insufficient balance."); 
+        balances[msg.sender] -= amount; 
+        balances[recipient] += amount; 
+    }
+}
